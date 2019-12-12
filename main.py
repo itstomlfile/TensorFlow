b5b294ba59c30fcc56ae6cb1cdf2d3be7bc3b153
Tom <tom.charlesworth1997@gmail.com>
2019-12-12 14:55:20 +0000

    Classify fashion items

--- main.py
+++ main.py
@@ -27,3 +27,12 @@ model.fit(train_images, train_labels, epochs=5)
 test_loss, test_acc = model.evaluate(test_images,test_labels)
 
 print("Tested Acc: ", test_acc)
+
+prediction = model.predict(test_images[7])
+
+for i in range(5):
+    plt.grid(False)
+    plt.imshow(test_images[i], cmap=plt.cm.binary)
+    plt.xlabel("Actual: " + class_names[test_labels[i]])
+    plt.title("Prediction: " + class_names[np.argmax(prediction[i])])
+    plt.show()
